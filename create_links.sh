fa9e4f4ddbe734eda6116519ab2685b87a35900d
Subrahmanya Lingappa <subrahmanya.lingappa@qti.qualcomm.com>
2026-03-26 10:22:56 +0530

    Add create links script
    Signed-off-by: Subrahmanya Lingappa <subrahmanya.lingappa@qti.qualcomm.com>

---
+++ create_links.sh
@@ -0,0 +1,8 @@
+ln -s dotfiles/git/.gitconfig .gitconfig
+ln -s dotfiles/tmux/.tmux/	.tmux/
+ln -s dotfiles/tmux/.tmux.conf .tmux.conf
+ln -s dotfiles/vim/.vim/	.vim
+ln -s dotfiles/vim/.vimrc .vimrc
+ln -s dotfiles/zsh/.zshenv	.zshenv
+ln -s dotfiles/zsh/.zshrc  .zshrc.pre-oh-my-zsh
+
---
+++ push_command.sh
@@ -0,0 +1,1 @@
+ GIT_SSH_COMMAND="ssh -i  ~/.ssh/slingappa_git/id_rsa -F /dev/null" git push
